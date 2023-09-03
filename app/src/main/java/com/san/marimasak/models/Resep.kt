5cc9415a2f76ef60cbd4924cdb367aa7a856e8c7
Ihsan Hadiansyach <ihadiansyach@gmail.com>
2023-09-03 20:36:27 +0700

    feat data to parcelable

--- app/src/main/java/com/san/marimasak/models/Resep.kt
+++ app/src/main/java/com/san/marimasak/models/Resep.kt
@@ -2,37 +2,41 @@ package com.san.marimasak.models
 
 import android.os.Parcel
 import android.os.Parcelable
-import kotlinx.parcelize.Parceler
-import kotlinx.parcelize.Parcelize
 
-@Parcelize
 data class Resep(
     val namaMakanan: String?,
     val bahan: String?,
-    val foto: Int?,
+    val foto: Int,
     val bumbuHalus: String?,
     val caraMasak: String?
 ) : Parcelable {
     constructor(parcel: Parcel) : this(
         parcel.readString(),
         parcel.readString(),
-        parcel.readValue(Int::class.java.classLoader) as? Int,
+        parcel.readInt(),
         parcel.readString(),
-        parcel.readString()
-    )
+        parcel.readString()) {
+    }
 
-    companion object : Parceler<Resep> {
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(namaMakanan)
+        parcel.writeString(bahan)
+        parcel.writeInt(foto)
+        parcel.writeString(bumbuHalus)
+        parcel.writeString(caraMasak)
+    }
 
-        override fun Resep.write(parcel: Parcel, flags: Int) {
-            parcel.writeString(namaMakanan)
-            parcel.writeString(bahan)
-            parcel.writeValue(foto)
-            parcel.writeString(bumbuHalus)
-            parcel.writeString(caraMasak)
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Resep> {
+        override fun createFromParcel(parcel: Parcel): Resep {
+            return Resep(parcel)
         }
 
-        override fun create(parcel: Parcel): Resep {
-            return Resep(parcel)
+        override fun newArray(size: Int): Array<Resep?> {
+            return arrayOfNulls(size)
         }
     }
 }
